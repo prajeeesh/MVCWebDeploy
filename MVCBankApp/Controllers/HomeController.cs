0ced0559b9fab2844c45e4628fc245bbfa1ce3c9
Prajeesh <prajeesh.kottayam@objectconsulting.com>
2016-05-18 14:22:42 +1000

    First Commit

---
+++ MVCBankApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BankAccountNS;
+
+namespace MVCBankApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            BankAccount ba = new BankAccount("Mr. Baker", 1200.50);
+
+            ba.Credit(5.77);
+            string accountDetails = "Customer Name: " + ba.CustomerName + " and Balance :" + ba.Balance;
+            ViewBag.AccDetails = accountDetails;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
